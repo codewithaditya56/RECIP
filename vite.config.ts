8b34ef0cae7022359f8faa2a661001c6a02382bf
nandini-1109 <nandinikaushik1109@gmail.com>
2025-08-19 18:58:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { mochaPlugins } from "@getmocha/vite-plugins";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/RECIP/',  // 👈 VERY important for GitHub Pages
-})
-
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  plugins: [...mochaPlugins(process.env as any), react(), cloudflare()],
+  server: {
+    allowedHosts: true,
+  },
+  build: {
+    chunkSizeWarningLimit: 5000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+// base: "/RECIP/",//
+});
